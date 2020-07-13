3f7e4e658d9ccc9e49f66e9caeffe1ca1a137378
riazzam <sfhcls@gmail.com>
2020-07-13 10:00:52 +0200

    Primer commit

---
+++ PrimeroVariables/src/main/java/es/ltc/primeraprueba/PrimeroVariables/Basicos.java
@@ -0,0 +1,6 @@
+package es.ltc.primeraprueba.PrimeroVariables;
+
+public interface Basicos {
+	 public boolean esPar(int y);
+	 public boolean esImpar(int y);
+}
---
+++ PrimeroVariables/src/main/java/es/ltc/primeraprueba/PrimeroVariables/BasicosImpl.java
@@ -0,0 +1,22 @@
+package es.ltc.primeraprueba.PrimeroVariables;
+/**
+ * Primeros ejercicios de manejo de variables y operadores
+ * @author Leticia
+ *
+ */
+public class BasicosImpl implements Basicos {
+
+	@Override
+	public boolean esPar(int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean esImpar(int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
